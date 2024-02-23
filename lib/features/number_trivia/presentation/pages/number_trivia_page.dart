2b3591751b8b8754c72d340e12f67326af76c5a1
Jose Guerrero <51868977+Angel609-code@users.noreply.github.com>
2024-02-23 07:30:00 -0600

    first commit

---
+++ lib/features/number_trivia/presentation/pages/number_trivia_page.dart
@@ -0,0 +1,60 @@
+import 'package:clean_arquitecture/features/number_trivia/presentation/bloc/bloc.dart';
+import 'package:clean_arquitecture/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
+import 'package:clean_arquitecture/features/number_trivia/presentation/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../injection_container.dart';
+
+class NumberTriviaPage extends StatelessWidget {
+  const NumberTriviaPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Number Trivia'),
+      ),
+      body: SingleChildScrollView(
+        child: buildBody(context),
+      ),
+    );
+  }
+
+  BlocProvider<NumberTriviaBloc> buildBody(BuildContext context) {
+    return BlocProvider(
+      create: (_) => sl<NumberTriviaBloc>(),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: <Widget>[
+              const SizedBox(height: 10),
+              // Top half
+              BlocBuilder<NumberTriviaBloc, NumberTriviaState>(
+                builder: (context, state) {
+                  if (state is Empty) {
+                    return MessageDisplay(
+                      message: 'Start searching!',
+                    );
+                  } else if (state is Loading) {
+                    return LoadingWidget();
+                  } else if (state is Loaded) {
+                    return TriviaDisplay(numberTrivia: state.trivia);
+                  } else if (state is Error) {
+                    return MessageDisplay(
+                      message: state.message,
+                    );
+                  }
+                },
+              ),
+              const SizedBox(height: 20),
+              // Bottom half
+              TriviaControls()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
